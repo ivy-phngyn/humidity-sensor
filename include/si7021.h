3ddd3480fe1ded7cab0e5f167df1d85dcfc1121f
ivy-phngyn <ivy@phngyn.dev>
2025-12-17 23:07:02 -0500

    updating macros

--- include/si7021.h
+++ include/si7021.h
@@ -7,9 +7,12 @@
 #include "freertos/task.h"
 #include <stdint.h>
 
-#define SI7021_SENSOR_ADDR          0x40
-#define I2C_MASTER_TIMEOUT_MS       1000
-#define MEASURE_RH_NHMM             0xF5
+#define SI7021_SENSOR_ADDR          0x40     /*!< Address for SI7021 sensor */
+#define I2C_MASTER_TIMEOUT_MS       1000     /*!< I2C Master Timeout */
+#define MEASURE_RH_NHMM             0xF5     /*!< Command to measure relative humidity - no clock stretching */
+#define I2C_MASTER_SCL_IO           22       /*!< GPIO number used for I2C master clock */
+#define I2C_MASTER_SDA_IO           21       /*!< GPIO number used for I2C master data  */
+#define I2C_MASTER_FREQ_HZ          100000 /*!< I2C master clock frequency */
 
 /**
 * Initializes and configures ESP32 to be master & SI7021 sensor to be slave on I2C bus
--- src/si7021.c
+++ src/si7021.c
@@ -50,8 +50,8 @@ esp_err_t si7021_init(void)
     
     i2c_master_bus_config_t bus_config = {
         .i2c_port = I2C_NUM_0,
-        .sda_io_num = 21,
-        .scl_io_num = 22,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .scl_io_num = I2C_MASTER_SCL_IO,
         .clk_source = I2C_CLK_SRC_DEFAULT,
         .glitch_ignore_cnt = 7,
         .flags.enable_internal_pullup = true,
@@ -62,7 +62,7 @@ esp_err_t si7021_init(void)
     i2c_device_config_t dev_config = {
         .dev_addr_length = I2C_ADDR_BIT_LEN_7,
         .device_address = SI7021_SENSOR_ADDR,
-        .scl_speed_hz = 100000,
+        .scl_speed_hz = I2C_MASTER_FREQ_HZ,
     };
     err = i2c_master_bus_add_device(s_bus_handle, &dev_config, &s_dev_handle);
     return err;
